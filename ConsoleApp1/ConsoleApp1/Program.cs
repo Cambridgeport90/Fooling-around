a06648e4838ba503dc8522cc34ecff2b97c11e35
Katherine Moss <kmoss@winterhillsolutions.com>
2018-11-24 23:27:35 -0500

    This is the actual C# code changes.
    Whoops? Evidently I need to learn where my projects are initially going. Wow.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testInteger;
+            string myString;
+            testInteger = 17;
+            myString = "\"testInteger\" is";
+        Console.WriteLine($"{myString} {testInteger}");
+            Console.ReadKey();
+                            }
+    }
+}
